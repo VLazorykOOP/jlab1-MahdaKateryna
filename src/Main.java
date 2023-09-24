9f9839c3d452ebba16cabd7eced96ac5843d7df2
MahdaKateryna <mahda.kateryna@chnu.edu.ua>
2023-09-24 10:54:01 +0300

    task 2

--- src/Main.java
+++ src/Main.java
@@ -25,6 +25,41 @@ public class Main {
         System.out.println();
         System.out.println("\t\t\t Task 2");
 
+        System.out.print("Input n, n<400: ");
+        int n = in.nextInt();
+
+        int[] A = new int[n];
+        for(int i = 0; i < n; i++)
+        {
+            System.out.print("Input " + i + " element: ");
+            A[i] = in.nextInt();
+        }
+
+        int maxUnic = Integer.MIN_VALUE;
+
+        for (int i = 0; i < n; i++) {
+            int curNum = A[i];
+            boolean Unic = true;
+
+            // Перевірка на унікальність:
+            for (int j = 0; j < n; j++) {
+                if (i != j && curNum == A[j]) {
+                    Unic = false;
+                    break;
+                }
+            }
+
+            if (Unic && curNum > maxUnic) {
+                maxUnic = curNum;
+            }
+        }
+
+        if (maxUnic != Integer.MIN_VALUE) {
+            System.out.println("Max unique number: " + maxUnic);
+        } else {
+            System.out.println("No unique numbers.");
+        }
+
 
 
     }
